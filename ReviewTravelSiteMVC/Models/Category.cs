7c279eb413171203f4584d7c871a006e5f9beeeb
divnag <divyamn.shine@gmail.com>
2018-03-09 15:09:04 -0500

    first commit

---
+++ ReviewTravelSiteMVC/Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ReviewTravelSiteMVC.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryID { get; set; }
+        public string Location { get; set; }
+        public String About { get; set; }
+
+        //Navigation properties
+        public virtual ICollection<Review> Reviews { get; set; }
+
+    }
+}
---
+++ ReviewTravelSiteMVC/Models/Review.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace ReviewTravelSiteMVC.Models
+{
+    public class Review
+    {
+
+            [Key]
+            public int ReviewID { get; set; }
+            public string ReviewerName { get; set; }
+             [Display (Name="Title Of Your Review")]
+            public string ReviewTitle { get; set; }
+            [DisplayFormat(DataFormatString = "{0:yyyy/MM/dd}")]
+             public DateTime ReviewDate { get; set; }
+            public string ReviewContent { get; set; }
+            [Display (Name ="Recommended")]
+            public bool IsRecommended { get; set; }
+
+            [ForeignKey("Category")]
+            public int CategoryID { get; set; }
+
+            //Navigation Properties
+
+            public virtual Category Category { get; set; }
+
+
+
+        
+
+    }
+}
